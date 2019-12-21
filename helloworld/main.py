6d4b435cf0f9a90312c0a8c22f7b3345b2d3e50a
TEEN-BOOM <51919967+TEEN-BOOM@users.noreply.github.com>
2019-12-21 16:03:09 +0530

    Update main.py
    Python3(print_func)and other python best practices.

--- helloworld/main.py
+++ helloworld/main.py
@@ -1,9 +1,19 @@
+#!/usr/bin/python3
+# -*- coding: UTF-8 -*-
+"""main.py: console app which prints hello"""
 import sys
 
 def main(argv=None):
+    """says Hello, world
+       Returns: None
+    """
+
     if argv is None:
         argv = sys.argv
 
-    print "Hello, world"
+    print("Hello, world")
 
-    return 0
+    return None
+
+if __name__ == '__main__':
+    main()#testing
