d548f97352421c2ee54a6a56c07dd373056386e3
TEEN-BOOM <51919967+TEEN-BOOM@users.noreply.github.com>
2020-01-23 12:12:22 +0530

    Update main.py
    minor changes

--- helloworld/main.py
+++ helloworld/main.py
@@ -1,19 +1,20 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
-"""main.py: console app which prints hello"""
+"""main.py: A console app, it prints hello world"""
 import sys
 
-def main(argv=None):
-    """says Hello, world
+def main(argv=None) -> None:
+    """prints Hello, world
        Returns: None
     """
 
     if argv is None:
         argv = sys.argv
-
+        
     print("Hello, world")
 
     return None
 
 if __name__ == '__main__':
-    main()#testing
+    #test
+    main()
