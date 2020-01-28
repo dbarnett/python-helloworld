44b3bf7cd27808ac95fc395acf5fd5b7b6cf76a6
David Barnett <dbarnett@google.com>
2020-01-27 20:59:11 -0800

    Add --version and --help arguments using argparse

--- helloworld/main.py
+++ helloworld/main.py
@@ -1,8 +1,25 @@
+"""Top-level implementation of the helloworld program."""
+
+import argparse
 import sys
+
+import helloworld
+
+
+parser = argparse.ArgumentParser(
+        description='A simple example program to print a friendly greeting.')
+parser.add_argument('--version', action='version',
+        version='helloworld ' + helloworld.__version__)
+
 
 def main(argv=None):
     if argv is None:
         argv = sys.argv
+
+    # The helloworld program doesn't expect any arguments.
+    # This just checks for the special --version and --help arguments and
+    # ensures the user hasn't passed any other unrecognized arguments.
+    parser.parse_args(argv[1:])
 
     print("Hello, world")
 
--- setup.py
+++ setup.py
@@ -1,16 +1,22 @@
-import sys
+import io
+import os.path
 from setuptools import setup
+
+VERSION_PATH = os.path.join(
+    os.path.dirname(__file__), 'helloworld/VERSION.txt')
+with io.open(VERSION_PATH, 'r', encoding='utf-8') as f:
+  version = f.read().strip()
 
 setup(
     name = "helloworld",        # what you want to call the archive/egg
-    version = "0.1",
+    version = version,
     packages=["helloworld"],    # top-level python modules you can import like
                                 #   'import foo'
     dependency_links = [],      # custom links to a specific project
     install_requires=[],
     extras_require={},      # optional features that other packages can require
                             #   like 'helloworld[foo]'
-    package_data = {},
+    package_data = {"helloworld": ["VERSION.txt"]},
     author="David Barnett",
     author_email = "davidbarnett2@gmail.com",
     description = "The familiar example program in Python",
