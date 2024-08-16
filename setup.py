288d7ced1b971fd1b3b0c36002b96e1c3f91542e
David Barnett <david@mumind.me>
2024-08-16 17:45:24 -0600

    Move package metadata from setup.py into pyproject.toml
    Details on pyproject.toml: PEP 621.

--- setup.py
+++ setup.py
@@ -8,25 +8,5 @@ with io.open(VERSION_PATH, 'r', encoding='utf-8') as f:
   version = f.read().strip()
 
 setup(
-    name = "helloworld",        # what you want to call the archive/egg
     version = version,
-    packages=["helloworld"],    # top-level python modules you can import like
-                                #   'import foo'
-    dependency_links = [],      # custom links to a specific project
-    install_requires=[],
-    extras_require={},      # optional features that other packages can require
-                            #   like 'helloworld[foo]'
-    package_data = {"helloworld": ["VERSION.txt"]},
-    author="A. Developer",
-    author_email = "author@example.com",
-    description = "The familiar example program in Python",
-    license = "Apache 2.0",
-    keywords= "example documentation tutorial",
-    url = "http://github.com/dbarnett/python-helloworld",
-    entry_points = {
-        "console_scripts": [        # command-line executables to expose
-            "helloworld_in_python = helloworld.main:main",
-        ],
-        "gui_scripts": []       # GUI executables (creates pyw on Windows)
-    }
 )
