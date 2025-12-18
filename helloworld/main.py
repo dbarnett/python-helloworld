f80f6edc16d832abeb078c5051357994c59792cf
Kishore <kishorevadluri.2011@gmail.com>
2025-12-17 22:59:07 -0500

    [DE-1234] modified

--- helloworld/main.py
+++ helloworld/main.py
@@ -21,6 +21,6 @@ def main(argv=None):
     # ensures the user hasn't passed any other unrecognized arguments.
     parser.parse_args(argv[1:])
 
-    print("Hello, world1")
+    print("Hello, world2")
 
     return 0
